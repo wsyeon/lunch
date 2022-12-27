9e16e4041bc4720f7093096e9b384a4f9d0ca558
C팀 <sywon2005@gmail.com>
2022-12-27 13:03:54 +0900

    css

--- src/pages/Lunch.js
+++ src/pages/Lunch.js
@@ -10,6 +10,7 @@ const Lunch = () => {
   const [dinner, setDinner] = useState([]);
   const [lunchNull, setLunchNull] = useState();
   const [clicks, setClicks] = useState(false);
+  const [clicks2, setClicks2] = useState(false);
   const navigate = useNavigate();
   const time = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate() - 1}`;
   // console.log(api[rand].DDISH_NM);
@@ -25,11 +26,11 @@ const Lunch = () => {
       setInfo(resutl[0].DDISH_NM.split(""));
     }
     setClicks(true);
+    setClicks2(true);
   };
   const loading = ()=> {
     const infos = info.filter(infoss=> infoss !== " " && infoss !== '(' && infoss !== 'b' &&infoss !== 'r' && infoss !== '/' && infoss !== '<' & infoss !== '>' && infoss !== '.' && infoss !== '0' && infoss !== '1' && infoss !== '2' && infoss !== '3' && infoss !== '4' && infoss !== '5' && infoss !== '6' && infoss !== '7' && infoss !== '8' && infoss !== '9');
     const infos_result = infos.join("");
-    console.log(infos_result.split(')'));
     const final_result = infos_result.split(')');
     setDinner(final_result.filter(info=> info !== ''));
     setClicks(false);
@@ -42,21 +43,21 @@ const Lunch = () => {
     <div className='lunchWrapper'>
       <div className='lunchMenu'>
         {lunchNull !== null ? (
-          <div>
+          <div className='dinnerWrapper'>
             {dinner.map((item, index)=> (
               <ul className='dinner' key={index}>
                 <li>{item}</li>
               </ul>
             ))}
-            <button onClick={loading} className={clicks ? 'yes' : 'no'}>급식 확인하기</button>
           </div>
         ) : (
           <span>오늘은 급식이 업성요</span>
         )}
       </div>
       <div className='lunchBtn'>
-        <button onClick={click}>급식 불러오기</button>
-        <button onClick={goMain}>홈으로</button>
+        <button onClick={click} className={clicks2 ? 'no' : 'yes'}>급식 불러오기</button>
+        <button onClick={loading} className={clicks ? 'yes' : 'no'}>급식 확인하기</button>
+        <button className='yes' onClick={goMain}>홈으로</button>
       </div>
     </div>
   );
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,7 +6,7 @@ const Main = () => {
     return (
         <div className='MainWrapper'>
             <div className='imgWrapper'><img src='https://w.namu.la/s/5eb5f6440b3d2d4f2ab8bac8955c0b91e1cb2b1c6b784f13fad36d426fff721976e4ab6e6902c2ba07b3d9b96bbc3cd70bbd2dfb2d2e100c954ce9cfaf5519fcb12197516adaeb487a57ba9a6fe32c482457360da594fb69617500888e310f96' /></div>
-            <div className='link'><Link className='checkLunch' to='/lunch'>오늘의 점심 확인하기</Link></div>
+            <div className='link'><Link to='/lunch'><button className='checkLunch'>오늘의 점심 확인하기</button></Link></div>
         </div>
     );
 };
