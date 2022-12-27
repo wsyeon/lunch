a0ccdc41dbe3556ee1083e8528daff0aead18b21
C팀 <sywon2005@gmail.com>
2022-12-27 14:52:00 +0900

    2st complete

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../stlye/Main.css';
-import mainLogo from '../image/이대부고1.png';
+import mainLogo from '../image/\ ewuhs.png';
 
 const Main = () => {
     return (
         <div className='MainWrapper'>
-            <div className='imgWrapper'><img src={mainLogo} /></div>
+            <div className='imgWrapper'><img src={mainLogo} alt='이대부고 로고'/></div>
             <div className='link'><Link to='/lunch'><button className='checkLunch'>오늘의 점심 확인하기</button></Link></div>
         </div>
     );
